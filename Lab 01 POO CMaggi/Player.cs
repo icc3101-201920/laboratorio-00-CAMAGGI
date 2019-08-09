26954b92770c56ec4a32957f0fbd4b8641286cdf
CAMAGGI <43652021+CAMAGGI@users.noreply.github.com>
2019-08-09 10:29:37 -0400

    commit

---
+++ Lab 01 POO CMaggi/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_POO_CMaggi
+{
+    class Player
+    {
+        private int lifePoints;
+        private int attackPoints;
+        private Deck deck;
+        private Hand hand;
+        private Board board;
+        private SpecialCard Captain;
+        public Player(int LP, int AP, Deck D, Hand H, Board B, SpecialCard SC)
+        {
+        int lifePoints = LP;
+        int attackPoints = AP;
+        Deck deck = D;
+        Hand hand = H;
+        Board board = B;
+        SpecialCard Captain = SC;
+        }
+
+        public int LifePoints { get => lifePoints; set => lifePoints = value; }
+        public int Attackpoints { get => attackPoints; set => attackPoints = value; }
+
+        public Deck Deck { get => deck; set => deck = value; }
+        public Hand Hand { get => hand; set => hand = value; }
+
+
+        public void DrawCard()
+        {
+
+        }
+        public void PlayCard(int cardId)
+        {
+
+        }
+        public void ChangeCard(int cardId)
+        {
+
+        }
+        public void FirstHand()
+        {
+
+        }
+        public void ChooseCaptainCard(SpecialCard captainCard)
+        {
+
+
+        }
+
+    }
+
+
+}
---
+++ Lab 01 POO CMaggi/SpecialCard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_POO_CMaggi
+{
+    class SpecialCard
+    {
+        private string name;
+        private string type;
+        private string buffType;
+        private string effect;
+
+        public SpecialCard(string N, string T, string B, string E)
+        {
+            string name = N;
+            string type = T;
+            string buffType = B;
+            string effect = E;
+        }
+        public string Name { get => name; set => name = value; }
+        public string BuffType { get => buffType; set => buffType = value; }
+        public string Type { get => type; set => type = value; }
+        public string Effect { get => effect; set => effect = value; }
+
+    }
+
+}
